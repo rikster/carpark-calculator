c0d7d0fa20e3f37f0c5a156030f9287f5fd48f30
Richard Hounslow <rhounslow@gmail.com>
2020-09-29 06:57:53 +1000

    route updates

--- routes/standard.js
+++ routes/standard.js
@@ -5,6 +5,15 @@ const router = express.Router();
 // @desc        Get a standard price list
 // @access      Private
 router.get("/", (req, res) => {
-  res.send("Get standard price list");
+  res.send("Get a standard carpark  price list");
 });
+
+// @route    POST api/standard
+// @desc     Post a standard prices list
+// @access   Private
+router.post('/', (req, res) => {
+  res.send('Post a standard price list');
+});
+
+module.exports = router;
 
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // import syntax requires babel for es6 interpretation
-const express = require("express");
+const express = require('express');
 const app = express();
 
 //you can send html, json, files, etc
-app.get("/", (req, res) => res.send("Welcome to The Contact Calculator"));
-app.get("/welcome", (req, res) =>
-  res.json({ msg: "Welcome to the Carpark Calculator" })
+app.get('/', (req, res) => res.send('Welcome to The Contact Calculator'));
+app.get('/welcome', (req, res) =>
+  res.json({ msg: 'Welcome to the Carpark Calculator' })
 );
 
-//Define Routes in other dires/files
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/users.js", require("./routes/users"));
-//app.use('/api/normal', require('./routes/normal'));
+//Define Routes in other dirs/files
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/standard', require('./routes/standard'));
+//app.use("/api/users", require("./routes/users"));
 //app.use('/api/special', require('./routes/special'));
 
 const PORT = process.env.PORT || 5001;
